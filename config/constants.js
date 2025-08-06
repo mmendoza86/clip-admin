a50c6d482b545c4e06619e71ab5428e66c9119fc
mmendoza86 <oziel_ad86@hotmail.com>
2025-08-06 01:20:08 -0600

    cambiamos puerto 5501

--- config/constants.js
+++ config/constants.js
@@ -4,5 +4,5 @@ const path = require('path');
 module.exports = {
   REPORTES_DIR: path.join(__dirname, '../public/reportes'),
   MAX_REPORTES: 5,
-  SERVER_PORT: process.env.PORT || 5001,
+  SERVER_PORT: process.env.PORT || 5501,
 };
